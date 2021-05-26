b797ae35e81cb844ec1995ca8d2721dfd985ac9e
Argzon <argzon01@gmail.com>
2021-05-26 03:47:59 +0200

    Added search input in front-end

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
  */
 const url = 'https://randomuser.me/api/?results=12';
 const gallery = document.getElementById('gallery');
+const search = document.querySelector('.search-container');
 
 /**
  * Fetch Requests
@@ -18,21 +19,34 @@ fetch(url)
 //  */
 function generateUser(data) {
     for (let i = 0; i < data.results.length; i++) {
+        const user = data.results[i];
         const person =`
         <div class="card">
             <div class="card-img-container">
-                <img class="card-img" src="${data.results[i].picture.large}" alt="profile picture">
+                <img class="card-img" src="${user.picture.large}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
-                <p class="card-text">${data.results[i].email}</p>
-                <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="card-text">${user.email}</p>
+                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
             </div>
         </div>
         `;
         gallery.insertAdjacentHTML('beforeend', person);
     }
 }
+
+// Search input
+function searchInput() {
+    const searchInput = `
+        <form action="#" method="get">
+            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+        </form>
+    `;
+    search.innerHTML = searchInput;
+}
+searchInput();
 
 /**
  * Event Listeners
